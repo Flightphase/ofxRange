5dcf7b1b6cf091d6bb792f77567492baec5e2fe7
obviousjim <james@jamesgeorge.org>
2011-12-25 14:51:17 -0800

    added comparison features and operator overloading

--- ofRange.h
+++ ofRange.h
@@ -3,11 +3,9 @@
  *  SFEditor
  *
  *  Created by Jim on 1/15/11.
- *  Copyright 2011 FlightPhase. All rights reserved.
  *
  */
 
-//JG Added for slider ranges
 //----------------------------------------------------------
 // ofRange
 //----------------------------------------------------------
@@ -16,6 +14,11 @@
 class ofRange {
   public:
 	virtual ~ofRange(){}
+	
+	ofRange( const ofRange& rg){
+		min = rg.min;
+		max = rg.max;
+	}
 	
 	ofRange( float _min=0.0f, float _max=1.0f){
 		min = _min;
@@ -43,10 +46,39 @@ class ofRange {
         return (min != rg.min) || (max != rg.max);
     }
 	
+	bool contains(float p){
+		return p >= min && p <= max;
+	}
+	
+	bool contains(const ofRange& rg){
+		return rg.min >= min && rg.max <= max;
+	}
+	
+	bool intersects(const ofRange& rg){
+		return contains(rg.min) || contains(rg.max);
+	}
+	
 	float span(){
 		return max - min;
 	}
 	
+	//union
+	ofRange operator+( const ofRange& rg ) const;
+	ofRange& operator+=( const ofRange& rg );
+	//intersection
+	ofRange operator-(const ofRange& rg) const;
+	ofRange& operator-=(const ofRange& rg);
+
+	
+	ofRange operator+( const float f ) const;
+	ofRange& operator+=( const float f );
+	ofRange operator-( const float f) const;
+	ofRange& operator-=(const float f);
+	
+	
+	friend ostream& operator<<(ostream& os, const ofRange& rg);
+	friend istream& operator>>(istream& is, const ofRange& rg);
+
     union  {
         struct {
             float min;
@@ -55,3 +87,62 @@ class ofRange {
         float r[2];
     };	
 };
+
+//ofVec2f
+
+inline ofRange ofRange::operator+( const ofRange& rg ) const {
+	return ofRange(MIN(min, rg.min), MAX(max, rg.max));
+}
+
+inline ofRange& ofRange::operator+=( const ofRange& rg ){
+	min = MIN(min, rg.min);
+	max = MAX(max, rg.max);
+	return *this;
+}
+
+inline ofRange ofRange::operator-( const ofRange& rg) const {
+//	if(!ofRange::intersects(rg)) return ofRange(0,0);
+	return ofRange(MAX(min, rg.min), MIN(max, rg.max));
+}
+
+inline ofRange& ofRange::operator-=( const ofRange& rg ) {
+//	if(!ofRange::intersects(rg)) return ofRange(0,0);
+	min = MAX(min, rg.min);
+	max = MIN(max, rg.max);
+	return *this;
+}
+
+inline ofRange ofRange::operator+( const float f ) const{
+	return ofRange(min+f, max+f);
+}
+
+inline ofRange& ofRange::operator+=( const float f ){
+	min += f;
+	max += f;
+	return *this;
+}
+
+inline ofRange ofRange::operator-( const float f) const{
+	return ofRange(min-f, max-f);
+}
+
+inline ofRange& ofRange::operator-=(const float f) {
+	min -= f;
+	max -= f;
+	return *this;
+}
+
+inline ostream& operator<<(ostream& os, const ofRange& rg) {
+	os << "[" << rg.min << " - " << rg.max << "]";
+	return os;
+}
+
+inline istream& operator>>(istream& is, ofRange& rg) {
+	is.ignore(1);
+	is >> rg.min;
+	is.ignore(3);
+	is >> rg.max;
+	is.ignore(1);
+	return is;
+}
+
